bf3d6df68aeaa352260f45cfb96c7e2531567819
admin <victordavi13jupiter@gmail.com>
2025-11-05 15:50:37 -0300

    Apenas alguns toques, ainda no começei a implementar nada

--- backendSpringBoot/src/main/java/com/example/demo/LibraryApplication.java
+++ backendSpringBoot/src/main/java/com/example/demo/LibraryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryApplication {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
     }
 }
